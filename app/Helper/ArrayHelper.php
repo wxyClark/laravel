0ea0cf3c78978ff29436aca15cd6a22b93b13c60
wxyClark <C18666211369@outlook.com>
2023-02-01 16:24:54 +0800

    base log

--- app/Helper/ArrayHelper.php
+++ app/Helper/ArrayHelper.php
@@ -11,7 +11,26 @@ class ArrayHelper
      * @param $param
      * @return array
      */
-    static public function paramsItemToArray($param){
+    public static function paramsItemToArray($param)
+    {
         return is_array($param) ? $param : [$param];
+    }
+
+    /**
+     * @desc 后去异常的基础信息
+     * @param  \Throwable  $throwable
+     * @param  string  $title
+     * @return array
+     */
+    public static function getThrowableInfo(\Throwable $throwable, $title = '')
+    {
+        return [
+            'title' => $title,
+            'code'  => $throwable->getCode(),
+            'file'  => $throwable->getFile(),
+            'line'  => $throwable->getLine(),
+            'msg'   => $throwable->getMessage(),
+            //  TODO 获取上一层的调用入口
+        ];
     }
 }
--- app/Http/Controllers/AbcDemo/BusinessNameController.php
+++ app/Http/Controllers/AbcDemo/BusinessNameController.php
@@ -4,10 +4,13 @@
 namespace App\Http\Controllers\AbcDemo;
 
 use App\Enums\ErrorCodeEnums;
+use App\Helper\ArrayHelper;
 use App\Http\Controllers\Controller;
 use App\Requests\AbcDemo\BusinessName\IndexRequest;
 use App\Services\AbcDemo\BusinessNameService;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Log;
 
 /**
  * @desc Demo控制器
@@ -17,9 +20,13 @@ class BusinessNameController extends Controller
 {
     protected $service;
 
-    public function __construct(BusinessNameService $service)
+    public function __construct(
+        BusinessNameService $service,
+        Request $request
+    )
     {
         $this->service = $service;
+        Log::info($request->url().' 入参', $request->input());
     }
 
     /**
@@ -60,6 +67,7 @@ class BusinessNameController extends Controller
     public function detail(Request $request)
     {
         try {
+            Log::info('');
             $rules = [
                 'tenant_id' => ['required|integer|min:0', '租户ID'],
                 'uniq_code' => ['required|integer|min:0', '业务编码'],
@@ -68,6 +76,7 @@ class BusinessNameController extends Controller
 
             return $this->responseJson(ErrorCodeEnums::ERROR_CODE_DEFAULT, ['params' => $request->input()]);
         } catch (\Exception $e) {
+            Log::error(__METHOD__.' 异常', ArrayHelper::getThrowableInfo($e, 'BusinessName详情'));
             return $this->responseJson($e->getCode(), [], $e->getMessage());
         }
     }
